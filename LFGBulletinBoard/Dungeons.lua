d236cbd7427d63ad1e1369e261852d4ff382654d
Austin Little <Rapportus@gmail.com>
2024-01-19 11:27:21 -0600

    Add gadgetzan + everlook

--- LFGBulletinBoard/Dungeons.lua
+++ LFGBulletinBoard/Dungeons.lua
@@ -114,6 +114,8 @@ function GBB.GetDungeonNames()
 		["IRONFORGE"] = "Ironforge",
 		["SOUTHSHORE"] = "Southshore",
 		["RATCHET"] = "Ratchet",
+		["GADGETZAN"] = "Gadgetzan",
+		["EVERLOOK"] = "Everlook",
 		["DMF"] = "Darkmoon Faire",
 		["BOOTY"] = "Booty Bay",
 		["SUMMON"] = "Other Summons",
@@ -653,7 +655,7 @@ GBB.VanillaDungeonLevels ={
 	["AQ40"] = 	{60,60}, ["NAX"] = 	{60,60},
 	["MISC"]= {0,100},
 	["DEBUG"] = {0,100}, ["BAD"] =	{0,100}, ["TRADE"]=	{0,100}, ["SM2"] =  {28,42}, ["DM2"] =	{58,60}, ["DEADMINES"]={18,23},
-	["ASHENVALE"] = {0,100}, ["DARNASSUS"] = {0,100}, ["STORMWIND"] = {0,100}, ["IRONFORGE"] = {0,100}, ["SOUTHSHORE"] = {0,100}, ["RATCHET"] = {0,100}, ["DMF"] = {0,100}, ["BOOTY"] = {0,100}, ["SUMMON"] = {0,100},
+	["ASHENVALE"] = {0,100}, ["DARNASSUS"] = {0,100}, ["STORMWIND"] = {0,100}, ["IRONFORGE"] = {0,100}, ["SOUTHSHORE"] = {0,100}, ["RATCHET"] = {0,100}, ["GADGETZAN"] = {0,100}, ["EVERLOOK"] = {0,100}, ["DMF"] = {0,100}, ["BOOTY"] = {0,100}, ["SUMMON"] = {0,100},
 }
 
 GBB.PostTbcDungeonLevels = {
@@ -665,7 +667,7 @@ GBB.PostTbcDungeonLevels = {
 	["AQ40"] = 	{60,60}, ["NAX"] = 	{60,60},
 	["MISC"]= {0,100},
 	["DEBUG"] = {0,100}, ["BAD"] =	{0,100}, ["TRADE"]=	{0,100}, ["SM2"] =  {28,42}, ["DM2"] =	{58,60}, ["DEADMINES"]={16,24},
-	["ASHENVALE"] = {0,100}, ["DARNASSUS"] = {0,100}, ["STORMWIND"] = {0,100}, ["IRONFORGE"] = {0,100}, ["SOUTHSHORE"] = {0,100}, ["RATCHET"] = {0,100}, ["DMF"] = {0,100}, ["BOOTY"] = {0,100}, ["SUMMON"] = {0,100},
+	["ASHENVALE"] = {0,100}, ["DARNASSUS"] = {0,100}, ["STORMWIND"] = {0,100}, ["IRONFORGE"] = {0,100}, ["SOUTHSHORE"] = {0,100}, ["RATCHET"] = {0,100}, ["GADGETZAN"] = {0,100}, ["EVERLOOK"] = {0,100}, ["DMF"] = {0,100}, ["BOOTY"] = {0,100}, ["SUMMON"] = {0,100},
 }
 
 
@@ -714,7 +716,7 @@ GBB.PvpNames = {
 	"WSG", "AB", "AV", "EOTS", "WG", "SOTA", "ARENA",
 }
 
-GBB.Misc = {"MISC", "TRADE", "ASHENVALE", "DARNASSUS", "STORMWIND", "IRONFORGE", "SOUTHSHORE", "RATCHET", "DMF", "BOOTY", "SUMMON",}
+GBB.Misc = {"MISC", "TRADE", "ASHENVALE", "DARNASSUS", "STORMWIND", "IRONFORGE", "SOUTHSHORE", "RATCHET", "GADGETZAN", "EVERLOOK", "DMF", "BOOTY", "SUMMON",}
 
 GBB.DebugNames = {
 	"DEBUG", "BAD", "NIL",
