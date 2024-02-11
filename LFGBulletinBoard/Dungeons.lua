34728ac9be6074d1c056a741ca7d5abc9492a983
Austin Little <Rapportus@gmail.com>
2024-02-11 06:21:05 -0600

    fix SM mappings

--- LFGBulletinBoard/Dungeons.lua
+++ LFGBulletinBoard/Dungeons.lua
@@ -23,6 +23,7 @@ function GBB.GetDungeonNames()
 		["GNO"] = 	"Gnomeregan",
 		["RFK"] = 	"Razorfen Kraul",
 		["SM2"] =	"Scarlet Monastery",
+		["SMANY"] = "Scarlet Monastery (Any)",
 		["SMG"] = 	"Scarlet Monastery: Graveyard",
 		["SML"] = 	"Scarlet Monastery: Library",
 		["SMA"] = 	"Scarlet Monastery: Armory",
@@ -658,7 +659,7 @@ GBB.VanillaDungeonLevels ={
 	["AQ40"] = 	{60,60}, ["NAX"] = 	{60,60},
 	["MISC"]= {0,100},
 	["DEBUG"] = {0,100}, ["BAD"] =	{0,100}, ["TRADE"]=	{0,100}, ["SM2"] =  {28,42}, ["DM2"] =	{58,60}, ["DEADMINES"]={18,23},
-	["ASHENVALE"] = {0,100}, ["DARNASSUS"] = {0,100}, ["STORMWIND"] = {0,100}, ["IRONFORGE"] = {0,100}, ["SOUTHSHORE"] = {0,100}, ["DUSKWOOD"] = {0,100}, ["WPL"] = {0,100}, ["RATCHET"] = {0,100}, ["DESOLACE"] = {0,100}, ["GADGETZAN"] = {0,100}, ["EVERLOOK"] = {0,100}, ["DMF"] = {0,100}, ["BOOTY"] = {0,100}, ["SUMMON"] = {0,100},
+	["ASHENVALE"] = {0,100}, ["DARNASSUS"] = {0,100}, ["SMANY"] = {28, 42}, ["STORMWIND"] = {0,100}, ["IRONFORGE"] = {0,100}, ["SOUTHSHORE"] = {0,100}, ["DUSKWOOD"] = {0,100}, ["WPL"] = {0,100}, ["RATCHET"] = {0,100}, ["DESOLACE"] = {0,100}, ["GADGETZAN"] = {0,100}, ["EVERLOOK"] = {0,100}, ["DMF"] = {0,100}, ["BOOTY"] = {0,100}, ["SUMMON"] = {0,100},
 }
 
 GBB.PostTbcDungeonLevels = {
@@ -670,7 +671,7 @@ GBB.PostTbcDungeonLevels = {
 	["AQ40"] = 	{60,60}, ["NAX"] = 	{60,60},
 	["MISC"]= {0,100},
 	["DEBUG"] = {0,100}, ["BAD"] =	{0,100}, ["TRADE"]=	{0,100}, ["SM2"] =  {28,42}, ["DM2"] =	{58,60}, ["DEADMINES"]={16,24},
-	["ASHENVALE"] = {0,100}, ["DARNASSUS"] = {0,100}, ["STORMWIND"] = {0,100}, ["IRONFORGE"] = {0,100}, ["SOUTHSHORE"] = {0,100}, ["RATCHET"] = {0,100}, ["DUSKWOOD"] = {0,100}, ["WPL"] = {0,100}, ["DESOLACE"] = {0,100}, ["GADGETZAN"] = {0,100}, ["EVERLOOK"] = {0,100}, ["DMF"] = {0,100}, ["BOOTY"] = {0,100}, ["SUMMON"] = {0,100},
+	["ASHENVALE"] = {0,100}, ["DARNASSUS"] = {0,100}, ["SMANY"] = {28, 42}, ["STORMWIND"] = {0,100}, ["IRONFORGE"] = {0,100}, ["SOUTHSHORE"] = {0,100}, ["RATCHET"] = {0,100}, ["DUSKWOOD"] = {0,100}, ["WPL"] = {0,100}, ["DESOLACE"] = {0,100}, ["GADGETZAN"] = {0,100}, ["EVERLOOK"] = {0,100}, ["DMF"] = {0,100}, ["BOOTY"] = {0,100}, ["SUMMON"] = {0,100},
 }
 
 
@@ -708,7 +709,7 @@ GBB.TbcDungeonNames = {
 
 GBB.VanillDungeonNames  = {
 	"RFC", "WC" , "DM" , "SFK", "STK", "BFD", "GNO",
-    "RFK", "SMG", "SML", "SMA", "SMC", "RFD", "ULD",
+    "RFK", "SMANY", "SMG", "SML", "SMA", "SMC", "RFD", "ULD",
     "ZF", "MAR", "ST" , "BRD", "LBRS", "DME", "DMN",
     "DMW", "STR", "SCH", "UBRS", "MC", "ZG",
     "AQ20", "BWL", "AQ40", "NAX",
